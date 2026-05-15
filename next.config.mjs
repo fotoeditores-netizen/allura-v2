fe46c223364b11191847fb9d08197bed4fbb2e7f
Juan Patino <desarrollo.fotoeditores@gmail.com>
2026-05-15 12:39:55 -0500

    feat(i18n): wrap next config with next-intl plugin

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+// next.config.mjs
+import createNextIntlPlugin from "next-intl/plugin";
+
+const withNextIntl = createNextIntlPlugin();
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [],
+  },
+};
+
+export default withNextIntl(nextConfig);
