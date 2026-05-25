60f8e00f103e3387a9734faaa725356ed1266b32
Juan Patino <desarrollo.fotoeditores@gmail.com>
2026-05-25 13:08:11 -0500

    feat(sanity): register all schema types in index.ts

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,3 +1,56 @@
-import { globalConfig } from "./singletons/globalConfig";
+// Objects (register before singletons and documents that use them)
+import { localeString, localeStringShort, localeText, localePortableText } from './objects/localeString'
+import { seoObject } from './objects/seoObject'
+import { ctaObject } from './objects/ctaObject'
+import { navItem } from './objects/navItem'
+import { processStep } from './objects/processStep'
 
-export const schemaTypes = [globalConfig];
+// Singletons
+import { siteSettings } from './singletons/siteSettings'
+import { navigation } from './singletons/navigation'
+import { homePage } from './singletons/homePage'
+import { trackingScripts } from './singletons/trackingScripts'
+
+// Documents
+import { serviceCategory } from './documents/serviceCategory'
+import { service } from './documents/service'
+import { page } from './documents/page'
+import { blogPost } from './documents/blogPost'
+import { category } from './documents/category'
+import { testimonial } from './documents/testimonial'
+import { faq } from './documents/faq'
+import { galleryItem } from './documents/galleryItem'
+import { video } from './documents/video'
+import { caseStudy } from './documents/caseStudy'
+import { teamMember } from './documents/teamMember'
+import { popup } from './documents/popup'
+
+export const schemaTypes = [
+  // Objects first
+  localeString,
+  localeStringShort,
+  localeText,
+  localePortableText,
+  seoObject,
+  ctaObject,
+  navItem,
+  processStep,
+  // Singletons
+  siteSettings,
+  navigation,
+  homePage,
+  trackingScripts,
+  // Documents
+  serviceCategory,
+  service,
+  page,
+  blogPost,
+  category,
+  testimonial,
+  faq,
+  galleryItem,
+  video,
+  caseStudy,
+  teamMember,
+  popup,
+]
